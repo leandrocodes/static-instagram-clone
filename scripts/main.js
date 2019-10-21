6ee47d19c6d4aa8e2d7e99203a88b21bcc2ec042
leandrocodes <leaxviana140@gmail.com>
2019-10-21 00:47:00 -0400

    modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
+const picDetail = document.querySelector('#pic-detail')
+const posts = document.querySelector('#posts')
+
 const createImage = src => {
   const image = document.createElement('img')
   image.src = src
   return image
 }
 
-const posts = document.querySelector('#posts')
+const onPostClick = event => {
+  const image = createImage(event.currentTarget.src)
+  document.body.classList.add('no-scroll')
+  picDetail.style.top = window.pageYOffset + 'px'
+  picDetail.appendChild(image)
+  picDetail.classList.remove('hidden')
+}
 
+// Put pics into DOM
 for(pic in picList){
   const picSRC = picList[pic]
   const image = createImage(picSRC)
+  image.addEventListener('click', onPostClick)
   posts.appendChild(image)
 }
